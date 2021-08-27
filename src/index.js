8f43978398824d9f11232b0aaa7cceb0c479cdc0
tanzila-abedin <tanzila.abedin@yahoo.com>
2021-08-27 15:43:08 +0200

    Add city search resource to retrieve key to find weather for a specific city or location

--- src/index.js
+++ src/index.js
@@ -1,13 +1,31 @@
-import _ from 'lodash';
+// import _ from 'lodash';
 
 
-function component() {
-  const element = document.createElement("div");
+// function component() {
+//   const element = document.createElement("div");
 
-  // Lodash, currently included via a script, is required for this line to work
-  element.innerHTML = _.join(["Hello", "webpack"], " ");
+//   // Lodash, currently included via a script, is required for this line to work
+//   element.innerHTML = _.join(["Hello", "webpack"], " ");
 
-  return element;
-}
+//   return element;
+// }
 
-document.body.appendChild(component());
+// document.body.appendChild(component());
+
+const apiKey = "iiT5pz1slpmXaLAWU1cygqoLpwoAeAu3";
+
+const findCity = async (city) => {
+  const cityResource =
+    "http://dataservice.accuweather.com/locations/v1/cities/search";
+
+  const queryKey = `?apikey=${apiKey}&q=${city}`;
+
+  const response = await fetch(cityResource + queryKey);
+  const data = await response.json();
+
+  return data[0];
+};
+
+findCity("johannesburg")
+.then(data => console.log(data))
+.catch(err => console.log(err));

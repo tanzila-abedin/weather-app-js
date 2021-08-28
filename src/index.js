064112910293b13c74c5b5be00f92e75542c0fe3
tanzila-abedin <tanzila.abedin@yahoo.com>
2021-08-28 12:50:02 +0200

    Get weather data using weather api

--- src/index.js
+++ src/index.js
@@ -14,18 +14,32 @@
 
 const apiKey = "iiT5pz1slpmXaLAWU1cygqoLpwoAeAu3";
 
+const findWeather = async (locId) => {
+  const currentCondRes = `http://dataservice.accuweather.com/currentconditions/v1/`
+
+  const queryParam = `${locId}?apikey=${apiKey}`;
+
+  const response = await fetch(currentCondRes + queryParam)
+  const data = await response.json()
+
+  return (data[0])
+}
+
 const findCity = async (city) => {
   const cityResource =
     "http://dataservice.accuweather.com/locations/v1/cities/search";
 
-  const queryKey = `?apikey=${apiKey}&q=${city}`;
+  const queryParam = `?apikey=${apiKey}&q=${city}`;
 
-  const response = await fetch(cityResource + queryKey);
+  const response = await fetch(cityResource + queryParam);
   const data = await response.json();
 
   return data[0];
 };
 
 findCity("johannesburg")
-.then(data => console.log(data))
-.catch(err => console.log(err));
+  .then(data =>{
+  return findWeather(data.Key)
+}).then(data => {
+  console.log(data)
+}).catch((err) => console.log(err));

c4f9949d26cdb717b4329fd61ebd6b5504b04f5b
tanzila-abedin <tanzila.abedin@yahoo.com>
2021-08-28 21:17:38 +0200

    remove commented code

--- src/index.js
+++ src/index.js
@@ -15,29 +15,6 @@ const addCity = async (city) => {
   }
 }
 
-// const farenHeitBtn = () => {
-//   const fBtn = document.getElementById('fbtn')
-//   fBtn.addEventListener('click',(e) => {
-//     e.preventDefault()
-//     alert('btn works')
-//       // const cBtn = document.getElementById("cBtn");
-//       // cBtn.className("d-block");
-//       // const fahrenheit = document.querySelector(".fahrenheit");
-//       // fahrenheit.className("d-block");
-//   })
-//   // celciusBtn()
-// }
-
-// // const celciusBtn = () => {
-// //   const cBtn = document.getElementById('cBtn')
-// //   cBtn.addEventListener('click',() => {
-// //             cBtn.className = "d-none";
-// //             const celcius = document.querySelector(".celcius");
-// //             celcius.className = "d-block";
-
-// //   } )
-// // }
-
 const display = (info)=> {
   const cityKey = info.cityKey
   const cityWeather = info.cityWeather
@@ -46,10 +23,8 @@ const display = (info)=> {
                    <h5 class="location">${cityKey.EnglishName}</h5>
                    <div>${cityWeather.WeatherText}</div>
                    <div>
-                        <span class="celcius">${cityWeather.Temperature.Metric.Value} &deg;C</span>
-                        <button id="cBtn" class="d-none"> Celcius</button>
-                        <span class="d-none fahrenheit">${cityWeather.Temperature.Imperial.Value} &deg;F</span>
-                        <button id="fBtn"> Farenheit</button>
+                        <span class="celcius">${cityWeather.Temperature.Metric.Value} &deg;C</span></br>
+                        <span id="fahrenheit">${cityWeather.Temperature.Imperial.Value} &deg;F</span>
                    </div>`;
   
 
@@ -61,34 +36,9 @@ const display = (info)=> {
   if (displayCard.classList.contains('d-none')){
     displayCard.classList.remove('d-none')
   }
-  farenHeitBtn()
+
 } 
 
-const farenHeitBtn = () => {
-  const fBtn = document.getElementById('fBtn')
-  fBtn.addEventListener('click',(e) => {
-    e.preventDefault();
-    fBtn.className = "d-none";
-    const fahrenheit = document.querySelector(".fahrenheit");
-    fahrenheit.className = "d-block";
-    const cBtn = document.getElementById("cBtn");
-    cBtn.className = "d-block";
-    const celcius = document.querySelector('.celcius')
-    celcius.className = 'd-none'
-  })
-  celciusBtn()
-}
-
-const celciusBtn = () => {
-  const cBtn = document.getElementById("cBtn");
-  cBtn.addEventListener("click", () => {
-    // cBtn.className = "d-none";
-    const cBtn = document.querySelector(".cBtn");
-    cBtn.className = "d-block";
-    const fBtn = document.getElementById(".fBtn");
-    fBtn.className = "d-block";
-  });
-};
 
 
 searchForm.addEventListener('submit', e => {
@@ -101,7 +51,7 @@ searchForm.addEventListener('submit', e => {
    .then(data => {
      display(data)
    }).catch(err => {
-     console.log(err)
+      display(err)
    })
 })
 
